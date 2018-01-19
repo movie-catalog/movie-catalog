a877da55eab52d9b1a1721916c0516f9971e9ace
Muhammad Haekal <93haekal@gmail.com>
2018-01-19 11:08:45 +0700

    add searchMovie function with fetchUrl generator

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
-const paragraphData = $("#data")
-const currencyCode = $("#curreny-code")
-const currencyName = $("#curreny-name")
-const currencySymbol = $("#curreny-symbol")
+// const paragraphData = $("#data")
+// const currencyCode = $("#curreny-code")
+// const currencyName = $("#curreny-name")
+// const currencySymbol = $("#curreny-symbol")
 
 // URL: https://restcountries.eu/rest/v2/alpha/id
 // {code: "IDR", name: "Indonesian rupiah", symbol: "Rp"}
 
-fetch('https://restcountries.eu/rest/v2/alpha/ind')
-  .then(response => response.json()).then(data => {
-    // GET CURRENCY ONLY
-    const currency = data.currencies[0]
+// fetch('https://restcountries.eu/rest/v2/alpha/ind')
+//   .then(response => response.json()).then(data => {
+//     // GET CURRENCY ONLY
+//     const currency = data.currencies[0]
+//
+//     // INSERT DATA INTO PARAGRAPH
+//     paragraphData.html(JSON.stringify(currency))
+//
+//     // PROPRAGATE DATA INTO INPUT BOXES
+//     currencyCode.val(currency.code)
+//     currencyName.val(currency.name)
+//     currencySymbol.val(currency.symbol)
+//   });
 
-    // INSERT DATA INTO PARAGRAPH
-    paragraphData.html(JSON.stringify(currency))
+function searchMovie(title = null, type = null, year = null) {
+  var fetchUrl = `http://www.omdbapi.com/?`
+  var isFirstParam = true
 
-    // PROPRAGATE DATA INTO INPUT BOXES
-    currencyCode.val(currency.code)
-    currencyName.val(currency.name)
-    currencySymbol.val(currency.symbol)
-  });
+  if(title !== null){
+    fetchUrl = fetchUrl + `s=${title}`
+    isFirstParam = false;
+  }
+  if (type !== null){
+    (isFirstParam) ? (fetchUrl = fetchUrl + `type=${type}`) : (fetchUrl = fetchUrl + `&type=${type}`)
+  }
+  if (year !== null){
+    (isFirstParam) ? (fetchUrl = fetchUrl + `y=${year}`) : (fetchUrl = fetchUrl + `&y=${year}`)
+  }
+
+  fetchUrl = fetchUrl + `&apikey=fbe6dc54&`
+
+  console.log(fetchUrl);
+
+  // fetch('')
+  //   .then(response => response.json()).then(data => {
+  //     // GET CURRENCY ONLY
+  //     const currency = data.currencies[0]
+  //
+  //     // INSERT DATA INTO PARAGRAPH
+  //     paragraphData.html(JSON.stringify(currency))
+  //
+  //     // PROPRAGATE DATA INTO INPUT BOXES
+  //     currencyCode.val(currency.code)
+  //     currencyName.val(currency.name)
+  //     currencySymbol.val(currency.symbol)
+  //   });
+
+}
